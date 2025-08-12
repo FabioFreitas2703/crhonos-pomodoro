efce028bda927b27429c282af7a5112fe78195f6
Fabio Silva <fabiosfreitas2703@gmail.com>
2025-08-12 20:34:21 -0300

    two commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './styles/theme.css'
+import './styles/global.css'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+export function App(){
+    console.log("oi");
+
+    return ( <div>
+        <Heading/>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam inventore harum dolorem sequi illum autem alias eaque natus! Placeat mollitia similique eligendi ullam possimus quisquam suscipit molestias porro. Est, sapiente.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <h2>Fabio Silva de Freitas</h2>
+        <h3>Vinicius Freitas</h3>
+    </div>
+    );
 }
 
-export default App
